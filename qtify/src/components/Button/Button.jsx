b80017153bc2e492f9de6df5d202ab06ff8f0f0e
Shree Nandan Das <nayan@Shrees-MacBook-Air.local>
2024-12-29 15:20:17 +0530

    Completed first milestone

--- qtify/src/components/Button/Button.jsx
+++ qtify/src/components/Button/Button.jsx
@@ -3,9 +3,9 @@ import styles from "./Button.module.css";
 
  function Button({children}) {
     return (
-        <div className={styles.button}>
-         <button>{children}</button>
-        </div>
+        
+         <button className={styles.button}>{children}</button>
+    
        
     )
 }
